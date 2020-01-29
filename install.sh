4d18e396b0431cc02a81bfb0510262e2dbe6ee26
schspa <schspa@gmail.com>
2020-01-29 22:43:08 +0800

    Add configure for macos
    Signed-off-by: schspa <schspa@gmail.com>

--- install.sh
+++ install.sh
@@ -11,9 +11,24 @@ fi
 ln -sf ${current_path}/tmux.conf $HOME/.tmux.conf
 
 # setup qutuebrowser
-ln -sf ${current_path}/qutebrowser/config/config.py $HOME/.config/qutebrowser/config.py
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	# ~/.config/qutebrowser/config.py
+	mkdir -p $HOME/.config/qutebrowser/
+	ln -sf ${current_path}/qutebrowser/config/config.py $HOME/.config/qutebrowser/config.py
+    # set up i3wm
+	ln -sf ${current_path}/config/i3 $HOME/.config/i3
+	ln -sf ${current_path}/config/polybar $HOME/.config/polybar
+	ln -sf ${current_path}/config/mpd $HOME/.config/mpd
+	ln -sf ${current_path}/config/autorandr $HOME/.config/autorandr
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	# ~/.qutebrowser/config.py
+	mkdir -p $HOME/.qutebrowser/
+	ln -sf ${current_path}/qutebrowser/config/config.py $HOME/.qutebrowser/config.py
+elif [[ "$OSTYPE" == "msys" || "OSTYPE" == "cygwin" || "OSTYPE" == "win32" ]]; then
+	# %APPDATA%/qutebrowser/config/config.py
+    # I'm not sure this can happen.
+	# Not test on windows yet
+else:
+    # Unknown.
+fi
 
-ln -sf ${current_path}/config/i3 $HOME/.config/i3
-ln -sf ${current_path}/config/polybar $HOME/.config/polybar
-ln -sf ${current_path}/config/mpd $HOME/.config/mpd
-ln -sf ${current_path}/config/autorandr $HOME/.config/autorandr
