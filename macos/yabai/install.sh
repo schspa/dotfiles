30e378f5e0f9348fb863a2e4394a77ea26d985bb
schspa <schspa@gmail.com>
2020-07-15 13:19:21 +0800

    add spacebar settings
    Signed-off-by: schspa <schspa@gmail.com>

--- macos/yabai/install.sh
+++ macos/yabai/install.sh
@@ -2,3 +2,5 @@ current_path=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 ln -sf ${current_path}/yabairc ${HOME}/.yabairc
 ln -sf ${current_path}/skhdrc ${HOME}/.skhdrc
+mkdir -p ${HOME}/.config/spacebar
+ln -sf ${current_path}/spacebarrc ${HOME}/.config/spacebar/spacebarrc
