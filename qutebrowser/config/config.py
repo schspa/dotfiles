899be849db0436e927e48bc9bf23183860632e28
schspa <schspa@iMac.lan>
2020-07-18 22:08:26 +0800

    qutebrowser: use meta rather than alt, for macos compatible.
    Signed-off-by: schspa <schspa@iMac.lan>

--- qutebrowser/config/config.py
+++ qutebrowser/config/config.py
@@ -33,13 +33,13 @@ ESC_BIND = 'clear-keychain ;; search ;; fullscreen --leave'
 c.bindings.commands['normal'] = {
 	# Navigation
 	'<ctrl-v>': 'scroll-page 0 0.5',
-	'<alt-v>': 'scroll-page 0 -0.5',
+	'<meta-v>': 'scroll-page 0 -0.5',
 	'<ctrl-shift-v>': 'scroll-page 0 1',
-	'<alt-shift-v>': 'scroll-page 0 -1',
+	'<meta-shift-v>': 'scroll-page 0 -1',
 	# FIXME come up with logical bindings for scrolling left/right
 
 	# Commands
-	'<alt-x>': 'set-cmd-text :',
+	'<meta-x>': 'set-cmd-text :',
 	'<ctrl-x>b': 'set-cmd-text -s :buffer',
 	'<ctrl-x>k': 'tab-close',
 	'<ctrl-x><ctrl-c>': 'quit',
@@ -59,7 +59,7 @@ c.bindings.commands['normal'] = {
 
 	# open links
 	'<ctrl-l>': 'set-cmd-text -s :open',
-	'<alt-l>': 'set-cmd-text -s :open -t',
+	'<meta-l>': 'set-cmd-text -s :open -t',
 	'<ctrl-x>f': 'set-cmd-text -s :open -t',
 
 	# editing
@@ -69,11 +69,11 @@ c.bindings.commands['normal'] = {
 	'<ctrl-e>': 'fake-key <End>',
 	'<ctrl-n>': 'fake-key <Down>',
 	'<ctrl-p>': 'fake-key <Up>',
-	'<alt-f>': 'fake-key <Ctrl-Right>',
-	'<alt-b>': 'fake-key <Ctrl-Left>',
+	'<meta-f>': 'fake-key <Ctrl-Right>',
+	'<meta-b>': 'fake-key <Ctrl-Left>',
 	'<ctrl-d>': 'fake-key <Delete>',
-	'<alt-d>': 'fake-key <Ctrl-Delete>',
-	'<alt-backspace>': 'fake-key <Ctrl-Backspace>',
+	'<meta-d>': 'fake-key <Ctrl-Delete>',
+	'<meta-backspace>': 'fake-key <Ctrl-Backspace>',
 	'<ctrl-w>': 'fake-key <Ctrl-backspace>',
 	'<ctrl-y>': 'insert-text {primary}',
 
@@ -102,8 +102,8 @@ c.bindings.commands['command'] = {
 	'<ctrl-p>': 'completion-item-focus prev',
 	'<ctrl-n>': 'completion-item-focus next',
 
-	'<alt-p>': 'command-history-prev',
-	'<alt-n>': 'command-history-next',
+	'<meta-p>': 'command-history-prev',
+	'<meta-n>': 'command-history-next',
 
 	# escape hatch
 	'<ctrl-g>': 'leave-mode',
