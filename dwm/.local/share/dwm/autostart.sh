0b3e7faafca9c5661800c8c2cc63238e03b29a7b
schspa <schspa@gmail.com>
2020-08-18 23:46:29 +0800

    dwm: add polkit
    Signed-off-by: schspa <schspa@gmail.com>

--- dwm/.local/share/dwm/autostart.sh
+++ dwm/.local/share/dwm/autostart.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
 killall dwmblocks; dwmblocks &
+killall /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 blueman-applet &
 nm-applet &
