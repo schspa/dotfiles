c7b06c8d103888141586e8d7d195b165b46379fc
schspa <schspa@gmail.com>
2020-08-19 00:08:26 +0800

    dwm: add volumeicon
    Signed-off-by: schspa <schspa@gmail.com>

--- dwm/.local/share/dwm/autostart.sh
+++ dwm/.local/share/dwm/autostart.sh
@@ -3,5 +3,6 @@
 killall dwmblocks; dwmblocks &
 killall /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+killall volumeicon; volumeicon &
 blueman-applet &
 nm-applet &
