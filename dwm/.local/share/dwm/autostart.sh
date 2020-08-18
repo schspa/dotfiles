e97a37b45990cf9f9cbc49c94500abcd10158452
schspa <schspa@gmail.com>
2020-08-19 00:33:27 +0800

    dwm: add auto start apps
    Signed-off-by: schspa <schspa@gmail.com>

--- dwm/.local/share/dwm/autostart.sh
+++ dwm/.local/share/dwm/autostart.sh
@@ -4,5 +4,9 @@ killall dwmblocks; dwmblocks &
 killall /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 killall volumeicon; volumeicon &
+xss-lock -- i3lock-fancy &
 blueman-applet &
 nm-applet &
+syncthing -no-browser &
+usr/lib/gsd-xsettings &
+compton -b &
