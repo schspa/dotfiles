e8a692037ef136ef2d30acc6d1f14af768a87dd0
schspa <schspa@gmail.com>
2021-09-06 00:27:54 +0800

    update for dwm.
    Signed-off-by: schspa <schspa@gmail.com>

--- dwm/.local/share/dwm/autostart.sh
+++ dwm/.local/share/dwm/autostart.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
 
-killall dwmblocks; dwmblocks &
-killall /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+killall dwmstatus 2>/dev/null; dwmstatus &
+killall /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 2>/dev/null
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-killall pa-applet; pa-applet &
+killall pa-applet 2>/dev/null; pa-applet &
 xss-lock -- i3lock-fancy &
 blueman-applet &
 nm-applet &
 syncthing -no-browser &
-usr/lib/gsd-xsettings &
+/usr/lib/gsd-xsettings &
 compton -b &
+killall fcitx5 2>/dev/null; fcitx5 &
+##
